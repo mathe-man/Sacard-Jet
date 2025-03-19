b7f4e5d911ca5f18faf842177bf5258dd701b9cb
mathe-man <mathieu.workcontact@gmail.com>
2025-03-19 12:23:01 +0300

    Add constructors for Body and engine class, added methods to Body class"

--- SacardJetEngine.cs
+++ SacardJetEngine.cs
@@ -4,10 +4,32 @@ namespace SacardJet;
 
 public class SacardJetEngine
 {
-    private readonly string Name = "SacardJetEngine";
+    private readonly string Name = "SacardJet Engine";
 
-    public readonly float G;
-    public readonly float AirResitance;
+    public readonly float G = 6.6743e-11f;
+    public readonly float AirResitance = 0f;
+
+    public List<Body> Bodies;
+
+    public SacardJetEngine(string name = "SacardJet Engine", float g = 6.6743e-11f, float airResitance = 0f, List<Body>? bodies = null, bool initMessage = false)
+    {
+        Name = name;
+        
+        G = g;
+        AirResitance = airResitance;
+        Bodies = bodies ?? new List<Body>();
+        
+        if (initMessage)
+        {Console.WriteLine($"SacardJetEngine '{Name}' initMessage, initialized with G={G}, AirResitance={AirResitance}, and {Bodies.Count} Bodies");}
+    }
+
+    /// <summary>
+    /// Update method update the state of each bodies in the current bodies list of the instance
+    /// </summary>
+    /// <returns>Bodies list after the update</returns>
+    public List<Body> Update()
+    {        
+    }
 }
 
 
@@ -22,6 +44,35 @@ public class Body
     public float Radius { get; set; }
     
     public Vector3 Color { get; set; }
-    
+
+
+    public Body(Vector3 position, float radius, float mass, Vector3 velocity, Vector3 color)
+    {
+        if (radius <= 0 || mass <= 0)
+        {
+            throw new Exception("Body radius and mass must be greater than zero");
+        }
         
+        
+        Position = position;
+        Velocity = velocity;
+        Force = new();
+        
+        Mass = mass;
+        Radius = radius;
+        
+        Color = color;
+
+    }
+    
+    public bool IsCollided(Body other) => Vector3.Distance(Position, other.Position) < Radius +  other.Radius;
+    public static bool IsCollided(Body a, Body b) => a.IsCollided(b);
+
+    public string StringInfo(string separator = ";")
+    {
+        //Info are: position, velocity, force, mass, radius
+        string text = $"[position: {Position.X},{Position.Y},{Position.Z}]{separator}[velocity: {Velocity.X},{Velocity.Y},{Velocity.Z}]{separator}[force: {Force.X},{Force.Y},{Force.Z}]{separator}[mass: {Mass}]{separator}[radius: {Radius}]{separator}";
+        return text;
+    }
+    
 }
